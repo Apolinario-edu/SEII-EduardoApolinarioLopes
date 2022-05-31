7d54ff804f15e32118308fddecbfe6d569dd9046
Eduardo Lopes <eduardo-apl@hotmail.com>
2022-05-30 23:38:12 -0300

    Exercicios Video 03

---
+++ Semana04/py03.py
@@ -0,0 +1,47 @@
+# Integers and Floats
+
+num = 3
+
+print(type(num)) #type retorna o tipo da variavel
+ 
+# Division 3 / 2 O resultado contem o decimal 
+# Floor Division 3 // 2 O resultado n contem o decimal
+
+# Incrementando uma variavel
+
+num = num + 1
+
+print(num)
+
+num += 1 
+
+num *= 2
+
+print(num)
+
+print(round(3.75)) # Arredondando para o proximo valor inteiro mais proximo
+print(round(3.75, 1)) #Arredondando para o proximo digito depois do decimal
+
+#Comparações ( Retornan True or False )
+
+#Equal   3 == 2 
+#Not Equal 3 != 2 
+#Greater than 3> 2 
+#Less Than 3 < 2 
+#Greater or Equal 3 >= 2 
+#Less or Equal 3 <=2
+
+num3 = 3
+num4 = 2
+
+print(num3 == num4)
+print(num3 != num4)
+print(num3 > num4)
+print(num3 < num4)
+print(num3 >= num4)
+print(num3 <= num4)
+
+
+
+       
+      
