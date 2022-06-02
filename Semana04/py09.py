659cffab712400fd3f417a9e3063c9f917ce6bc6
Eduardo Lopes <eduardo-apl@hotmail.com>
2022-06-02 09:56:08 -0300

    Exercicios video 09

---
+++ Semana04/py09.py
@@ -0,0 +1,12 @@
+from my_module import find_index, test  #Explorando Bibliotecas e importando 'modulos'
+import sys
+
+courses = courses = ['History', 'Math', 'Physics', 'CompSci']
+
+index = find_index(courses, 'Math')
+print(index)
+print(test)
+
+print(sys.path)
+
+
