55e50f53d77aa8c093cbedb83092176c7dad9c13
Eduardo Lopes <eduardo-apl@hotmail.com>
2022-06-02 10:29:41 -0300

    Exercicios videos 11

---
+++ Semana04/py11.py
@@ -0,0 +1,25 @@
+#Escrevendo e editando arquivos via Python
+
+with open('test.txt', 'r') as f  #Atenção onde se encontram os diretorios a serem editadas
+
+    size_to_read = 10   # tamanho a ser lido 
+    
+    #for line in f:
+     #       print(line, end='')
+
+    f_contentes = f.readline()
+    print(f_contents, end='')
+
+    f_contest = f.readline()
+    print(f_contents, end='')
+
+#Adicionando imagem
+
+whit open('image.jpg', 'rb' as rf:
+          whith open('image2.jpg','wb' as wf:
+                     chunk_size = 4000
+                     rf_chunk = rf.read(chunk_size)
+                     while len(rf_chunk)
+                     rf_chunk = rf.red(chunk_size)
+
+                     #Imagem adicionada aos arquivos. 
